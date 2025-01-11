491c871b72a1ebfe5c4d9531eb349134ee9f6710
saintares97 <rolonvan@gmail.com>
2025-01-11 12:57:32 -0500

    Added test cases to unit test file

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,25 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_different_text(self):
+        node = TextNode("Text node text", TextType.BOLD)
+        node2 = TextNode("Different text node text", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_different_texttype(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_different_url(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        node2 = TextNode("This is a text node", TextType.BOLD, "https://testingnodes.com")
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.NORMAL, "https://testingnodes.com")
+        self.assertEqual("TextNode(This is a text node, normal, https://testingnodes.com)", repr(node))
+
 
 if __name__ == "__main__":
     unittest.main()
