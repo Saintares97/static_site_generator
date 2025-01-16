c7829700bd09ae2a8eac8154d2a9734d41318404
saintares97 <rolonvan@gmail.com>
2025-01-16 13:30:52 -0500

    Created unit test for HTMLNode with 3 basic test cases

---
+++ src/test_htmlnode.py
@@ -0,0 +1,18 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html_single(self):
+        node = HTMLNode(props={"href": "https://google.com"})
+        expected = ' href="https://google.com"'
+        self.assertEqual(node.props_to_html(), expected)
+
+    def test_props_to_html_empty(self):
+        node = HTMLNode(props={})
+        expected = ""
+        self.assertEqual(node.props_to_html(), expected)
+
+    def test_props_to_html_multiple(self):
+        node = HTMLNode(props={"href": "https://google.com", "target": "_blank"})
+        expected = ' href="https://google.com" target="_blank"'
+        self.assertEqual(node.props_to_html(), expected)
